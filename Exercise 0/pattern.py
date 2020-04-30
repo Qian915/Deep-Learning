86461c5cbe6c58c8e8ce11ce0f7445285d94eef2
Qian915 <53493761+Qian915@users.noreply.github.com>
2020-04-30 23:24:27 +0200

    Add files via upload

--- Exercise 0/pattern.py
+++ Exercise 0/pattern.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 
 class Checker:
+    output = np.ndarray
     def __init__(self, resolution, tile_size):
         self.resolution = resolution
         self.tile_size = tile_size
-        self.output = np.zeros(resolution, resolution)
 
     def draw(self):
         if(self.resolution % (2 * self.tile_size) == 0):
@@ -14,16 +14,13 @@ class Checker:
             rep_times = self.resolution / self.tile_size / 2
             base = np.array([[1,0], [0,1]])
             self.output = np.tile(base, (rep_times, rep_times))
-            #color
-            checkerboard_color = LinearSegmentedColormap.from_list('checkerboard_cmap', [(0,0,0), (1,1,1)])
-            self.output = plt.imshow(self.output, cmap=checkerboard_color)
             copy = self.output
             return copy
 
     def show(self):
         #color
-            checkerboard_color = LinearSegmentedColormap.from_list('checkerboard_cmap', [(0,0,0), (1,1,1)])
-            self.output = plt.imshow(self.output, cmap=checkerboard_color)
+        checkerboard_color = LinearSegmentedColormap.from_list('checkerboard_cmap', [(0,0,0), (1,1,1)])
+        plt.imshow(self.output, cmap=checkerboard_color)
         plt.show()
 
 class Circle:
