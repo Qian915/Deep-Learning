70406f3271707c1f6206d8feabe7a6ed92afbed2
Qian915 <53493761+Qian915@users.noreply.github.com>
2020-05-21 17:38:54 +0200

    Update FullyConnected.py

--- Exercise 1/Layers/FullyConnected.py
+++ Exercise 1/Layers/FullyConnected.py
@@ -9,8 +9,8 @@ class FullyConnected:
         self.input_size = input_size
         self.output_size = output_size
 
-        # private member: _optimizer
-        self._optimizer = None
+        # protected member: _optimizer
+        self._optimizer = Optimizers.Sgd(np.random.uniform(0, 1))
 
         self.grad_weights = None
 
@@ -18,7 +18,7 @@ class FullyConnected:
         self.weights = np.random.uniform(0, 1, (self.input_size+1, self.output_size))
 
         # x:(batch_size, input_size+1)
-        self.input = None
+        self.input = np.ndarray
 
     # property: optimizer, gradient_weights
     @property
@@ -34,26 +34,24 @@ class FullyConnected:
         return self.grad_weights
 
     def forward(self, input_tensor):
-        # input (+bias)
-        bias = np.ones((input_tensor.shape[0], 1))
-        self.input = np.concatenate((input_tensor, bias), axis=1)
+        input_tensor = np.array(input_tensor)    # input_tensor is of type tuple
+        # input with bias
+        bias = np.ones((np.shape(input_tensor)[0], 1))
+        self.input = np.append(input_tensor, bias, axis=1)
 
-        out = np.dot(self.input, self.weights)     # can't concatenate ??????????????????????????????????
+        out = np.dot(self.input, self.weights)     # (50,4) (5,3) ??????????????
         return out
 
     def backward(self, error_tensor):
-        # x_grad
-        x_grad = np.dot(error_tensor, self.weights.T)
+        error_tensor = np.array(error_tensor)     # error_tensor is of type tuple
+        # x_grad w/o bias
+        weights_no_bias = self.weights[0:-1, :]
+        error = np.dot(error_tensor, weights_no_bias.T)
 
         # w_grad
         self.grad_weights = np.dot(self.input.T, error_tensor)
 
         # update weights
-        self.optimizer = Optimizers.Sgd(1e-3)
         self.weights = self.optimizer.calculate_update(self.weights, self.gradient_weights)
 
-        return x_grad, self.gradient_weights
-
-
-
-
+        return error
