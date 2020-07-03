058a8c7d21d44a91c2b44e642814527165ab249d
Qian Chen <qian.chen0915@gmail.com>
2020-07-03 09:20:21 +0200

    Start exercise 3!

---
+++ Exercise 3/Layers/__init__.py
@@ -0,0 +1,2 @@
+Conv.py__all__ = ["Helpers", "FullyConnected", "SoftMax", "ReLU", "Flatten", "TanH", "Sigmoid", "RNN", "LSTM",
+           "Conv", "Pooling", "Initializers", "Dropout", "BatchNormalization", "Base"]
