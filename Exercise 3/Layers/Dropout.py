bae2e1adf7c9be9f614cdda29821b825d5bf3819
Qian Chen <qian.chen0915@gmail.com>
2020-07-05 12:26:59 +0200

    Add Dropout layer!

--- Exercise 3/Layers/Dropout.py
+++ Exercise 3/Layers/Dropout.py
@@ -1,5 +1,22 @@
+import numpy as np
 from Layers.Base import BaseLayer
 
 
 class Dropout(BaseLayer):
-    pass
+    def __init__(self, probability):
+        super().__init__()
+        # p: keep!
+        self.probability = probability
+        self.activations = None
+
+    def forward(self, input_tensor):
+        if not self.testing_phase:
+            self.activations = np.random.binomial(n=1, p=self.probability, size=input_tensor.shape) / self.probability
+            input_tensor *= self.activations
+        # skip dropout layer in testing time
+        return input_tensor
+
+    def backward(self, error_tensor):
+        if not self.testing_phase:
+            error_tensor *= self.activations
+        return error_tensor
