ae170a5ff36a2bcaca4c33c010e31014600e0a36
Qian Chen <qian.chen0915@gmail.com>
2020-07-05 13:23:32 +0200

    Add BatchNormalization layer!

--- Exercise 3/Layers/BatchNormalization.py
+++ Exercise 3/Layers/BatchNormalization.py
@@ -1,5 +1,16 @@
+import numpy as np
 from Layers.Base import BaseLayer
+from Layers import Initializers
 
 
 class BatchNormalization(BaseLayer):
-    pass
+    def __init__(self, channels):
+        super().__init__()
+        self.channels = channels
+        self.weights, self.bias = self.initialize(Initializers.Constant(1), Initializers.Constant(0))
+
+ def initialize(self, weights_initializer, bias_initializer):
+        self.weights = weights_initializer.initialize((self.input_size, self.output_size), self.input_size, self.output_size)
+        self.bias = bias_initializer.initialize((1, self.output_size), self.input_size, self.output_size)
+
+        return self.weights, self.bias
