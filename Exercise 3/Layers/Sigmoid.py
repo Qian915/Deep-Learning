1156ea48cd8026d21b548042eb462092dbdc9d99
Qian Chen <qian.chen0915@gmail.com>
2020-07-08 14:51:41 +0200

    Add Sigmoid!

--- Exercise 3/Layers/Sigmoid.py
+++ Exercise 3/Layers/Sigmoid.py
@@ -1,5 +1,21 @@
+import numpy as np
 from Layers.Base import BaseLayer
 
 
 class Sigmoid(BaseLayer):
-    pass
+
+    def __init__(self):
+        super().__init__()
+        self.activations = None
+
+    def forward(self, input_tensor):
+        out = 1 / (1 + np.exp(-input_tensor))
+        self.activations = out
+        return out
+
+    def backward(self, error_tensor):
+        out = self.activations * (1 - self.activations)
+        return out * error_tensor
+
+
+
