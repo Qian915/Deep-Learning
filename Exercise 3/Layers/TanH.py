6d6eeeae84d55ad46bd9b63087af8da6a3fd5e33
Qian Chen <qian.chen0915@gmail.com>
2020-07-08 14:52:09 +0200

    Add TanH!

--- Exercise 3/Layers/TanH.py
+++ Exercise 3/Layers/TanH.py
@@ -1,5 +1,19 @@
+import numpy as np
 from Layers.Base import BaseLayer
 
 
 class TanH(BaseLayer):
-    pass
+
+    def __init__(self):
+        super().__init__()
+        self.activations = None
+
+    def forward(self, input_tensor):
+        out = np.tanh(input_tensor)
+        self.activations = out
+        return out
+
+    def backward(self, error_tensor):
+        out = 1 - self.activations ** 2
+        return out * error_tensor
+
