71ea7d503292a70ae5d4308459be8e10c6f9ed12
Qian Chen <qian.chen0915@gmail.com>
2020-07-09 23:34:05 +0200

    Add fc_h & fc_y layers for RNN!

--- Exercise 3/Layers/Base.py
+++ Exercise 3/Layers/Base.py
@@ -2,6 +2,6 @@ class BaseLayer:
     def __init__(self):
         self.testing_phase = False
         # set default weights
-        self.weights = None
+        # self.weights = None
 
 
--- Exercise 3/Layers/RNN.py
+++ Exercise 3/Layers/RNN.py
@@ -3,6 +3,7 @@ from Layers.Base import BaseLayer
 from Layers.TanH import TanH
 from Layers.FullyConnected import FullyConnected
 from Layers import Sigmoid
+from Layers import Initializers
 
 
 class RNN(BaseLayer):
@@ -11,64 +12,78 @@ class RNN(BaseLayer):
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
+        # hidden layer & output layer for RNN cell
+        self.fc_h = FullyConnected(self.input_size+self.hidden_size, self.hidden_size)
+        self.fc_y = FullyConnected(self.hidden_size, self.output_size)
+
         # initialize hidden_state with all 0
-        self.hidden_state = np.zeros((1, hidden_size))
-        self.output = None
-        self.memorize = False
-        self.weights = None
-        self.gradient_weights = None
-        self.optimizer = None
+        self.hidden_state = []
+        self.hidden_state.append(np.zeros((1, hidden_size)))
+
+        self.output = []
+        self._memorize = False
+        self._gradient_weights = None
+        self._optimizer = None
 
     @property
     def memorize(self):
-        return self.memorize
+        return self._memorize
 
     @memorize.setter
     def memorize(self, new_memorize):
-        self.memorize = new_memorize
+        self._memorize = new_memorize
 
     @property
     def weights(self):
-        return self.weights
+        return self.fc_h.weights
 
     @weights.setter
     def weights(self, new_weights):
-        self.weights = new_weights
+        self.fc_h.weights = new_weights
 
     @property
     def gradient_weights(self):
-        return self.gradient_weights
+        return self._gradient_weights
 
     @gradient_weights.setter
     def gradient_weights(self, new_grad_w):
-        self.gradient_weights = new_grad_w
+        self._gradient_weights = new_grad_w
 
     @property
     def optimizer(self):
-        return self.optimizer
+        return self._optimizer
 
     @optimizer.setter
     def optimizer(self, new_optimizer):
-        self.optimizer = new_optimizer
+        self._optimizer = new_optimizer
 
     def forward(self, input_tensor):
         for t in range(input_tensor.shape[0]):
-            if not self.memorize:
-                self.hidden_state = np.zeros((1, self.hidden_size))
+            if not self.memorize and t > 0:
+                self.hidden_state[t-1] = np.zeros((1, self.hidden_size))
             # ht
-            x_composed = np.concatenate((input_tensor[t], self.hidden_state, np.ones(1)))
-            input_size = x_composed.shape[0]
-            output_size = self.output_size
-            fc_h = FullyConnected(input_size, output_size)
-            self.hidden_state = TanH.forward(fc_h.forward(x_composed))
-            self.weights = fc_h.weights
+            x_composed = np.hstack((input_tensor[t], np.array(self.hidden_state[t-1 if t > 0 else 0])))
+            self.hidden_state[t] = TanH.forward(self.fc_h.forward(x_composed))
 
             # yt
-            input_size = self.hidden_state.shape[0]
-            fc_y = FullyConnected(input_size, output_size)
-            self.output = np.vstack(self.output, Sigmoid(fc_y.forward(self.hidden_state)))
+            self.output.append(Sigmoid(self.fc_y.forward(self.hidden_state[t])))
 
-        return self.output
+        self.hidden_state = np.array(self.hidden_state)
+        return np.array(self.output)
 
     def backward(self, error_tensor):
-        pass
+        # gradient w.r.t. x
+        # !!!!! not finished !!!!!
+        for t in range(error_tensor.shape[0]-1, -1, -1):
+            error_h = self.fc_y.backward(Sigmoid.backward(error_tensor))
+            gradient_x_composed = self.fc_h.backward(TanH.backward(error_h))
+            # split Xt, ht-1
+            error = gradient_x_composed[:, 0:self.input_size]
+
+        return error
+
+    def initialize(self, weights_initializer, bias_initializer):
+        self.weights = weights_initializer.initialize((self.input_size+self.hidden_size, self.output_size), self.input_size+self.hidden_size, self.output_size)
+        self.bias = bias_initializer.initialize((1, self.output_size), self.input_size+self.hidden_size, self.output_size)
+
+        self.weights = np.vstack((self.weights, self.bias))
